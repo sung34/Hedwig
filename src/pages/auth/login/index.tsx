6933d7a1786c3419d93adbcf104209a8489f4693
bongsee <ajinsuk3@naver.com>
2023-04-20 00:30:35 +0900

    :wrench: auth 관련 라우팅 구조 초기 세팅 및 의존성 패키지 추가
    - 추가된 패키지 : react-hook-form

---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,10 @@
+// Login Page도 Register Page와 마찬가지로 Auth Page로부터만 진입할 수 있게 설계.
+// Auth Page로 이동할 수 있는 뒤로가기 버튼 배치.
+
+import React from 'react'
+
+function LoginPage() {
+    return <div>LoginPage</div>
+}
+
+export default LoginPage
---
+++ src/pages/auth/register/index.tsx
@@ -0,0 +1,8 @@
+// 회원가입 페이지는 Auth Page를 통해서만 진입할 수 있게 설계
+// 로그인 페이지 링크는 페이지에서 제외하고, 대신 이전 페이지로 라우팅해주는 뒤로가기 버튼 배치.
+
+function RegisterPage() {
+    return <>회원가입 페이지</>
+}
+
+export default RegisterPage
