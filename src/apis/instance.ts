ec677249921cba117dca638bd13e80edda92c621
jyc-coder <jyc4648@gmail.com>
2023-04-21 22:18:29 +0900

    :wrench: feat: Instance 파일 생성
    제작한 api 를 사용해서 호출하는 메서드 생성

---
+++ src/apis/instance.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export const instance = axios.create({
+    baseURL: 'http://hedwigbe-env.eba-afs9gzxm.ap-northeast-2.elasticbeanstalk.com',
+    headers: {
+        'Content-Type': 'multipart/form-data',
+    },
+})
