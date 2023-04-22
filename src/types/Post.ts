a4701c36db0914eb0e3bf7993fcc400009054b2b
sung34 <imspins1234@gmail.com>
2023-04-23 02:41:21 +0900

    🔨 [Fix] types/Post.ts 수정 PostRequest interface의 title key값 삭제

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -2,13 +2,12 @@ import { User } from './User'
 import { Comment } from './Comment'
 
 export interface PostRequest {
-    title: string
     content: string
     img?: string
 }
 
 export interface Post extends PostRequest {
-    postId: string
+    id: number
     author: User
     likes: User[]
     comments: Comment[]
