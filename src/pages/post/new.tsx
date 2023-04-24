d4e2628980af696ade31e621d303e9a742c9d65f
ronieo <romelzen50@gmail.com>
2023-04-24 16:57:21 +0900

    í´¨fix: button 수정
    - button -> CustomButton으로 수정

--- src/pages/post/new.tsx
+++ src/pages/post/new.tsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import { Box, IconButton, TextField } from '@mui/material'
 import { ArrowBack, PhotoOutlined, VideoFileOutlined, Gif } from '@mui/icons-material'
 import { instance } from '@/apis/instance'
+import CustomButton from '@/components/CustomButton'
 
 function CreatePost() {
     const mainColor = '#5c940d'
     const [postInput, setPostInput] = useState({ body: '', img: new Blob() })
     const [previewUrl, setPreviewUrl] = useState('')
 
-    // 다른 확장자 추가를 시도할 시, 미디어가 교체되고 한 개의 미디어만 출력됩니다.
+    // 미디어 추가 함수
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value, files } = e.target
-        const file = e.target.files && e.target.files[0]
+        const file = files && files[0]
         if (files) {
             setPostInput({ ...postInput, [name]: files[0] })
         } else {
@@ -33,7 +34,7 @@ function CreatePost() {
         setPreviewUrl('')
     }
 
-    // 글쓰기에 추가한 내용 모두 저장할 함수
+    // 글쓰기에 추가한 내용 모두 저장 할 함수
     const handlePostSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const formData = new FormData()
@@ -52,14 +53,9 @@ function CreatePost() {
                         <ArrowBack />
                     </IconButton>
                     <form onSubmit={handlePostSubmit}>
-                        <button
-                            type="submit"
-                            id="hootButton"
-                            className="hootButton"
-                            style={{ backgroundColor: `${mainColor}`, color: 'white', borderRadius: '20px', border: `${mainColor}`, width: '5em', height: '2.8em' }}
-                        >
+                        <CustomButton type="submit" size="small" color="primary">
                             HOOT
-                        </button>
+                        </CustomButton>
                     </form>
                 </Box>
                 <TextField
@@ -77,12 +73,9 @@ function CreatePost() {
                 {previewUrl && (
                     <Box mb={2}>
                         <img src={previewUrl} alt="Preview" style={{ maxWidth: '50%', margin: '0 0.5em' }} />
-                        <button
-                            onClick={handleClearPreview}
-                            style={{ backgroundColor: 'white', color: `${mainColor}`, borderRadius: '20px', border: `1px solid ${mainColor}`, width: '5em', height: '2.8em' }}
-                        >
+                        <CustomButton onClick={handleClearPreview} size="small" color="secondary">
                             지우기
-                        </button>
+                        </CustomButton>
                     </Box>
                 )}
                 <Box display="flex" alignItems="center">
