d3fcfcba19df411ef7cdd35918aaa77ee9ab644d
ronieo <romelzen50@gmail.com>
2023-04-24 19:20:49 +0900

    í´¨fix: img S3 주소 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 
+if (!process.env.NEXT_PUBLIC_SERVER_URL) {
+    throw new Error('NEXT_PUBLIC_SERVER_URL is not defined')
+}
+
 const nextConfig = {
     reactStrictMode: true,
     env: {
         customKey: process.env.NEXT_PUBLIC_SERVER_URL,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fe4-react5-hedwig.s3.ap-northeast-2.amazonaws.com', // s3 서버 주소로 설정
+                port: '',
+                pathname: '**',
+            },
+        ],
     },
 }
 
