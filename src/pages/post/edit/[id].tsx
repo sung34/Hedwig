8a42b9c9af1f83685ee2cc1101743d69d5445654
jyc-coder <jyc4648@gmail.com>
2023-04-26 16:53:05 +0900

    :wrench: 수정 페이지 생성
    게시글의 morebtn을 클릭했을때 이동되는 페이지 생성

---
+++ src/pages/post/edit/[id].tsx
@@ -0,0 +1,41 @@
+import { getPost, getPosts } from '@/apis/Post'
+import { axiosInstance } from '@/apis/axios'
+import { Post } from '@/types/Post'
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
+import { useRouter } from 'next/router'
+import React from 'react'
+import CommentInput from '@/components/cards/commentInput'
+import PostCard from '@/components/cards/postCard'
+
+type Props = {
+    post: Post
+}
+// export const getStaticPaths: GetStaticPaths = async () => {
+//     const res = await fetch(process.env.customKey + `/post`)
+//     const data = await res.json()
+
+//     const paths = data.map((post: Post) => ({
+//         params: { id: post.id.toString() },
+//     }))
+
+//     return { paths, fallback: false }
+// }
+
+// export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+//     const id = Number(params?.id)
+//     const res = await fetch(process.env.customKey + `/post/${id}`)
+//     const data = await res.json()
+//     const post = data
+//     return {
+//         props: { post },
+//     }
+// }
+
+const editDetail = () => {
+    const router = useRouter()
+    const id = router.query.id
+
+    return <div>{/*<CommentInput profileImg={'/default.png'} userName={post.userName} />*/}</div>
+}
+
+export default editDetail
