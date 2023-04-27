7ce51b6c8ae8642fc4925eb6e457a4b636284bd3
sung34 <imspins1234@gmail.com>
2023-04-28 05:10:38 +0900

    🐛 [Fix] Global Theme palette가 액세스 되지않는 버그 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import { Snackbar } from '@mui/material'
 import type { AppProps } from 'next/app'
+import theme from '@/styles/styles'
 import { QueryClientProvider, QueryClient } from 'react-query'
-
+import { ThemeProvider } from '@mui/material'
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {},
@@ -10,7 +12,9 @@ const queryClient = new QueryClient({
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
         </QueryClientProvider>
     )
 }
