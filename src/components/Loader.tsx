094e656da94c76fc388aa5249da4e800a2332554
bongsee <ajinsuk3@naver.com>
2023-04-28 01:06:55 +0900

    :hammer: Loader 색상 메인색으로 변경

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import CircularProgress from '@mui/material/CircularProgress'
-import { useTheme } from '@mui/material'
 
 /**
  * @todo 영찬님 로더에도 적용할 수 있게 수정 요
  */
 function Loader() {
-    const theme = useTheme()
     return <CircularProgress color={'primary'} sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }} />
 }
 
